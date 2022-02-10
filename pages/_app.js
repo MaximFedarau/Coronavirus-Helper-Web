3ff8e62c4748a2e19fc3502c131d6a1109ace037
Maxim Fedarau <maxim@iMac-Maxim.local>
2022-02-10 21:42:00 +0300

    3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
+import {Provider} from "react-redux"
+import store from "../store/store"
+
 function MyApp({ Component, pageProps }) {
-  return <ChakraProvider>
+  return <Provider store={store}>
+    <ChakraProvider>
     <Component {...pageProps} />
   </ChakraProvider>
+  </Provider>
 }
 
 export default MyApp
